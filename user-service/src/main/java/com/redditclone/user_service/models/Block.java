fb820e0bd5f3c4ed1b9d881eb7833a97616efd39
karimgamaleldin <karimgamaleldin74@gmail.com>
2025-04-30 23:22:09 +0300

    feat: add block service

--- user-service/src/main/java/com/redditclone/user_service/models/Block.java
+++ user-service/src/main/java/com/redditclone/user_service/models/Block.java
@@ -2,11 +2,15 @@ package com.redditclone.user_service.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "blocks", uniqueConstraints = @UniqueConstraint(
                                                 name = "uq_blocks_blocker_blocked",
                                                 columnNames = {"blocker_id","blocked_id"})
@@ -17,11 +21,11 @@ public class Block {
     private Long id;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "blocker_id")
+    @JoinColumn(name = "blocker_id", nullable = false)
     private User blocker;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "blocked_id")
+    @JoinColumn(name = "blocked_id", nullable = false)
     private User blocked;
 
     public Block(User blocker, User blocked) {
--- user-service/src/main/java/com/redditclone/user_service/repositories/BlockRepository.java
+++ user-service/src/main/java/com/redditclone/user_service/repositories/BlockRepository.java
@@ -3,12 +3,15 @@ package com.redditclone.user_service.repositories;
 import com.redditclone.user_service.models.Block;
 import com.redditclone.user_service.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BlockRepository extends JpaRepository<Block, Long> {
     boolean existsByBlockerAndBlocked(User blocker, User blocked);
     void deleteByBlockerAndBlocked(User blocker, User blocked);
-    List<Block> findByBlocker(User blocker);
+    @Query("SELECT b.blocked FROM Block b WHERE b.blocker.id = :blockerId")
+    List<User> findBlockedUsersByBlockerId(@Param("blockerId") Long blockerId);
 }
 
