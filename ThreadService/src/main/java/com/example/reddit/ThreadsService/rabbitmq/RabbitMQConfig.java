b840bc440fb45d2c75efdd6fb742ec276d49f32b
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-05-07 02:26:09 +0300

    feat: added RabbitMQConfig, ReportRequest serialization and ThreadProducer

---
+++ ThreadService/src/main/java/com/example/reddit/ThreadsService/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package com.example.reddit.ThreadsService.rabbitmq;
+
+import com.example.reddit.ThreadsService.dto.ReportRequest;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String EXCHANGE = "thread-exchange";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        Map<String, Class<?>> idClassMapping = new HashMap<>();
+        idClassMapping.put("ReportRequest", ReportRequest.class);
+
+        typeMapper.setIdClassMapping(idClassMapping);
+        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
+        converter.setJavaTypeMapper(typeMapper);
+
+        return converter;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory cf, Jackson2JsonMessageConverter converter) {
+        RabbitTemplate tpl = new RabbitTemplate(cf);
+        tpl.setMessageConverter(converter);
+        return tpl;
+    }
+}
