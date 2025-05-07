5e2ca29f21eb7051a19ff2c35a9b041619d8a769
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-05-07 04:16:10 +0300

    fix: Restore CommunicationTestController for testing purposes

--- ThreadService/src/main/java/com/example/reddit/ThreadsService/controllers/CommunicationTestController.java
+++ ThreadService/src/main/java/com/example/reddit/ThreadsService/controllers/CommunicationTestController.java
@@ -1,27 +1,28 @@
 // FOR TESTING PURPOSES, DON'T REMOVE
-//package com.example.reddit.ThreadsService.controllers;
-//
-//import com.example.reddit.ThreadsService.rabbitmq.ThreadProducer;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.UUID;
-//
-//@RestController
-//@RequestMapping("/api/test/thread")
-//public class CommunicationTestController {
-//    private final ThreadProducer threadProducer;
-//
-//    @Autowired
-//    public CommunicationTestController(ThreadProducer threadProducer) {
-//        this.threadProducer = threadProducer;
-//    }
-//
-//    @GetMapping
-//    public void testThreadProducer() {
-//        UUID threadId = UUID.randomUUID();
-//        threadProducer.sendReportRequest(threadId, "test content");
-//    }
-//}
+
+package com.example.reddit.ThreadsService.controllers;
+
+import com.example.reddit.ThreadsService.rabbitmq.ThreadProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/test/thread")
+public class CommunicationTestController {
+    private final ThreadProducer threadProducer;
+
+    @Autowired
+    public CommunicationTestController(ThreadProducer threadProducer) {
+        this.threadProducer = threadProducer;
+    }
+
+    @GetMapping
+    public void testThreadProducer() {
+        UUID threadId = UUID.randomUUID();
+        threadProducer.sendReportRequest(threadId, "test content");
+    }
+}
