a4d813d2359b71aab706fddc7c7e41e29f03d829
karimgamaleldin <karimgamaleldin74@gmail.com>
2025-05-12 19:14:59 +0300

    feat: add new Notification Request DTO, Enum and adapting the CommunityProducer

---
+++ CommunitiesService/src/main/java/com/example/reddit/CommunitiesService/controllers/TestController.java
@@ -0,0 +1,35 @@
+package com.example.reddit.CommunitiesService.controllers;
+
+import com.example.reddit.CommunitiesService.models.Community;
+import com.example.reddit.CommunitiesService.rabbitmq.CommunityProducer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tests")
+public class TestController {
+    private CommunityProducer communityProducer;
+
+    public TestController(CommunityProducer communityProducer) {
+        this.communityProducer = communityProducer;
+    }
+
+    @GetMapping
+    public void test() {
+        UUID id = UUID.randomUUID();
+        String name = "kimo";
+        UUID topicId = UUID.randomUUID();
+        String description = "This is a test";
+        Community community = Community.builder()
+                .name(name)
+                .description(description)
+                .topicId(topicId)
+                .id(id)
+                .build();
+        communityProducer.notifyCommunity(community);
+    }
+}
